b342e3294ada7ba69e5fa442b2275b53b06643a5
Asger Pedersen <asgerp@users.noreply.github.com>
2023-12-01 17:16:00 +0100

    feat: day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,38 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var result = 0
+        input.forEach { s ->
+            val digs = s.filter { it.isDigit() }
+            val digits = "${digs.first()}${digs.last()}"
+            result += digits.toInt()
+            println("found ${digs.first()}${digs.last()}")
+            println("orig $digs")
+        }
+        return result
     }
+
+    val textToNumberMap = mapOf(
+        "One" to 1,
+        "Two" to 2,
+        "Three" to 3,
+        "Four" to 4,
+        "Five" to 5,
+        "Six" to 6,
+        "Seven" to 7,
+        "Eight" to 8,
+        "Nine" to 9
+    )
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    //val testInput = readInput("Day01_test")
+    //check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+    val input2 = readInput("Day01_02")
+    part2(input2).println()
 }
