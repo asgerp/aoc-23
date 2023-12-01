21229083b3f9e0083fcb25012b42a0c7718aabd2
Asger Pedersen <asger.pedersen@gmail.com>
2023-12-01 19:32:28 +0100

    solution for day1-2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,30 +1,57 @@
+import kotlin.system.measureTimeMillis
+
 fun main() {
     fun part1(input: List<String>): Int {
         var result = 0
         input.forEach { s ->
-            val digs = s.filter { it.isDigit() }
-            val digits = "${digs.first()}${digs.last()}"
+            val digits = s.filter { it.isDigit() }.let { d -> "${d.first()}${d.last()}" }
             result += digits.toInt()
-            println("found ${digs.first()}${digs.last()}")
-            println("orig $digs")
         }
         return result
     }
 
     val textToNumberMap = mapOf(
-        "One" to 1,
-        "Two" to 2,
-        "Three" to 3,
-        "Four" to 4,
-        "Five" to 5,
-        "Six" to 6,
-        "Seven" to 7,
-        "Eight" to 8,
-        "Nine" to 9
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9
     )
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val sum = input.fold(0) { accumulator,  line ->
+            val foundNumbersAtIndex = mutableListOf<Pair<Int, Int>>()
+            line.firstOrNull { it.isDigit() }.let {
+                val firstIndex = line.indexOfFirst { it.isDigit() }
+                foundNumbersAtIndex.add(it.toString().toInt() to firstIndex)
+            }
+            line.lastOrNull { it.isDigit() }.let {
+                val lastIndex = line.indexOfLast { it.isDigit() }
+                foundNumbersAtIndex.add(it.toString().toInt() to lastIndex)
+            }
+
+            for ((index, _) in line.withIndex()) {
+                if (index + 3 > line.length) continue
+                textToNumberMap[line.substring(index, index + 3)]?.let {
+                    foundNumbersAtIndex.add(it to index)
+                }
+                if (index + 4 > line.length) continue
+                textToNumberMap[line.substring(index, index + 4)]?.let {
+                    foundNumbersAtIndex.add(it to index)
+                }
+                if (index + 5 > line.length) continue
+                textToNumberMap[line.substring(index, index + 5)]?.let {
+                    foundNumbersAtIndex.add(it to index)
+                }
+            }
+            accumulator + (foundNumbersAtIndex.minBy { it.second }.first.toString() + "" +
+                    foundNumbersAtIndex.maxBy { it.second }.first.toString()).toInt()
+        }
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
@@ -33,6 +60,10 @@ fun main() {
 
     val input = readInput("Day01")
     part1(input).println()
-    val input2 = readInput("Day01_02")
-    part2(input2).println()
+    val elapsed = measureTimeMillis {
+        val input2 = readInput("Day01_02")
+        part2(input2).println()
+        check(part2(input2) == 54087)
+    }
+    println(elapsed)
 }
