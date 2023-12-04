dcbfdc6c493a05361a33acd2285a8b24a9f1032d
Asger Pedersen <asger.pedersen@gmail.com>
2023-12-04 18:07:50 +0100

    day4 part2

--- src/Day04.kt
+++ src/Day04.kt
@@ -2,10 +2,10 @@ import kotlin.math.pow
 import kotlin.system.measureTimeMillis
 
 fun main() {
-
     fun powerOfTwo(n: Int): Int {
         return 2.0.pow(n).toInt()
     }
+
     fun part1(input: List<String>): Int {
         var result = 0
         input.forEach { s ->
@@ -15,7 +15,7 @@ fun main() {
             val ticket = numbers[1].split(" ").filter { it.isNotBlank() }.map { it.toInt() }
             var power = 0
             winners.forEach {
-                if(ticket.contains(it)) {
+                if (ticket.contains(it)) {
                     power++
                 }
             }
@@ -28,6 +28,32 @@ fun main() {
 
     fun part2(input: List<String>): Int {
         var result = 0
+        // map of card => number of wins on the card
+        val winnerList =
+            buildMap {
+                for (i in 0..<input.size) {
+                    put(i, 1)
+                }
+            }.toMutableMap()
+        input.forEachIndexed { index, s ->
+            val line = s.split(":")
+            val numbers = line[1].split("|")
+            val winners = numbers[0].split(" ").filter { it.isNotBlank() }.map { it.toInt() }
+            val ticket = numbers[1].split(" ").filter { it.isNotBlank() }.map { it.toInt() }
+
+            var winnerCount = 0
+            winners.forEach {
+                // if the card contains a winning number
+                // add the current cards number of winners next winnerCount cards
+                if (ticket.contains(it)) {
+                    winnerCount++
+                    winnerList[index + winnerCount] = winnerList[index + winnerCount]!! + winnerList[index]!!
+                }
+            }
+            println(winnerList)
+            println("index: $index: - added ${winnerList[index]!!} to next $winnerCount")
+            result += winnerList[index]!!
+        }
         return result
     }
 
@@ -47,7 +73,7 @@ fun main() {
             val input2 = readInput("Day04_02")
             val part2Result = part2(input2)
             part2Result.println()
-            // check(part2Result == 78826761)
+            check(part2Result == 5489600)
         }
     println("part2 took: $elapsed2")
 }
